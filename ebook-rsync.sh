2b5ccad65e2183a10fe24f2ccb60165e250d8dd7
Cameron Lane <cameron@athena.(none)>
2012-03-04 20:11:36 -0500

    initial commit

---
+++ ebook-rsync.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+RSYNC=/usr/bin/rsync
+SSH=/usr/bin/ssh
+KEY=/home/cameron/cron/athena-rsync-key
+
+RUSER=cameron
+RHOST=adamanteus
+RPATH=/Users/cameron/Books/
+
+LPATH=/home/cameron/Books/
+
+# can later add physics medicine biology chemistry
+
+LIBS=(
+        ebooks 
+        programming 
+        mathematics
+     )
+
+for lib in ${LIBS[@]};
+do
+    $RSYNC -avu --delete -e "$SSH -i $KEY" $LPATH$lib $RUSER@$RHOST:$RPATH$index
+done
+
+exit 0
